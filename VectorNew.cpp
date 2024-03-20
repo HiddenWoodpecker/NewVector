fbc9dcf59264f66d87e3eca8a20be741900197a9
Матвей Однодворцев <mr.onv78@mail.ru>
2024-03-20 10:46:42 +0300

    Added realisation

--- VectorNew.cpp
+++ VectorNew.cpp
@@ -1,7 +1,48 @@
 #include "VectorNew.h"
 using ValueType = int;
 
-Vector::Vector(const ValueType* rawArray, const size_t size, float coef = 2.0f){
+Vector::Iterator::Iterator(ValueType* ptr){
+    _ptr = ptr;
+}
+ValueType* Vector::Iterator::operator->(){
+    return _ptr;
+}
+const ValueType* Vector::Iterator::operator->() const {
+    return _ptr;
+}
+
+Vector::Iterator Vector::Iterator::operator++(){
+    _ptr++;
+    return *this;
+}
+Vector::Iterator Vector::Iterator::operator++(int a){
+    _ptr += a;
+    return *this;
+}
+
+
+ValueType& Vector::Iterator::operator*(){
+    return *_ptr;
+}
+const ValueType& Vector::Iterator::operator*() const{
+    return *_ptr;
+}
+
+bool Vector::Iterator::operator==(const Iterator& other) const {
+    return (*this == other);
+}
+bool Vector::Iterator::operator!=(const Iterator& other) const {
+    return (*this != other);
+}
+Vector::Iterator Vector::begin(){
+    return Iterator(_data);
+}
+
+Vector::Iterator Vector::end(){
+    return Iterator(_data + _capacity);
+}
+
+Vector::Vector(const ValueType* rawArray, const size_t size, float coef){
     _data = new ValueType[size];
     _size = size;
     _capacity = size;
@@ -13,7 +54,7 @@ Vector::Vector(const ValueType* rawArray, const size_t size, float coef = 2.0f){
 }
 
 Vector::Vector(const Vector& other){
-    _data = new ValueType[other.size()];
+    _data = new ValueType[other._size];
     _size = other._size;
     _capacity = other._size;
     for (size_t i = 0; i < other._size; i++)
@@ -23,30 +64,161 @@ Vector::Vector(const Vector& other){
 }
 
 Vector& Vector::operator=(const Vector& other){
-    Vector vec = Vector(other);
-    return vec;
+    _data = new ValueType[other.size()];
+    _size = other._size;
+    _capacity = other._size;
+    for (size_t i = 0; i < other._size; i++)
+    {
+        _data[i] = other._data[i];
+    }
+    return *this;
 }
 
-
 Vector::Vector(Vector&& other) noexcept{
-    _data = other._data;
-    _size = other._size;
-    _capacity = other._capacity;
-    _multiplicativeCoef = other._multiplicativeCoef;
-    other._data = nullptr;
-    other._size = 0;
-    other._capacity = 0;
-    other._multiplicativeCoef = 2.0f;
+
+    if (this != &other){
+        _data = other._data;
+        _size = other._size;
+        _capacity = other._capacity;
+        _multiplicativeCoef = other._multiplicativeCoef;
+        delete other._data;
+    }
 }
 
 Vector& Vector::operator=(Vector&& other) noexcept{
-    Vector vec = Vector(other);
-    
+
+    if (this != &other){
+        _data = other._data;
+        _size = other._size;
+        _capacity = other._capacity;
+        _multiplicativeCoef = other._multiplicativeCoef;
+        delete other._data;
+    }
+    return *this;
+}
+
+ValueType& Vector::operator[](size_t idx){
+    if (idx < _size){
+        return _data[idx];
+    }
+    throw std::out_of_range("Out of range");
+}
+
+const ValueType& Vector::operator[](size_t idx) const{
+    if (idx < _size){
+        return _data[idx];
+    }
+    throw std::out_of_range("Out of range");
+}
+
+Vector::~Vector(){
+    delete _data;
+}
+
+long long Vector::find(const ValueType& value) const{
+    for (size_t i = 0; i < _size; i++)
+    {
+        if (*(_data + i) == value){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void Vector::reserve(size_t capacity){
+    if (_capacity < capacity){
+        _capacity = capacity;
+        ValueType* tmp = new ValueType[capacity];
+        for (size_t i = 0; i < _size; i++){
+            tmp[i] = _data[i];
+        }
+        delete _data;
+        _data = tmp;
+    }
 }
 
 void Vector::pushBack(const ValueType& value){
-
+    if (_size >= _capacity){
+        reserve(_capacity * _multiplicativeCoef);
+    }
+    _data[_size++] = value;
 }
+
+void Vector::insert(const ValueType& value, size_t pos){
+    if (pos <= _size){
+        if (_size >= _capacity){
+            reserve(_capacity * _multiplicativeCoef);
+        }
+        memmove(_data + pos + 1, _data+pos, sizeof(ValueType)*(_size - pos));
+        _data[pos] = value;
+        ++_size;
+    }
+}
+
+void Vector::insert(const ValueType* values, size_t size, size_t pos){
+    if (pos <= _size){
+        if (_size + size - 1 >= _capacity){
+            reserve((_capacity+size) * _multiplicativeCoef);
+        }
+        memmove(_data + pos + size, _data+pos, sizeof(ValueType)*(_size - pos));
+        for (size_t i = 0; i < size; i++){
+            _data[i+pos] = values[i];
+        }
+        _size += size;
+    }
+}
+
+void Vector::insert(const Vector& vector, size_t pos){
+    
+    if (pos <= _size){
+        size_t size = vector.size();
+        if (_size + size - 1 >= _capacity){
+            reserve((_capacity+size) * _multiplicativeCoef);
+        }
+        memmove(_data + pos + size, _data+pos, sizeof(ValueType)*(_size - pos));
+        for (size_t i = 0; i < size; i++){
+            _data[i+pos] = vector[i];
+        }
+        _size += vector.size();
+    }
+}
+
+void Vector::pushFront(const ValueType& value){
+    insert(value, 0);
+}
+
+void Vector::popBack(){
+    if (_size != 0){
+        --_size;
+    }
+}
+
+void Vector::popFront(){
+    if (_size != 0){
+        memmove(_data, _data + 1, _size);
+        --_size;
+    }
+}
+
+void Vector::erase(size_t pos, size_t count){
+    if(pos + count > _size){
+        _size = pos >= _size ? _size : pos;
+    }
+    else{
+        size_t difference = _size - pos - count;
+        for (size_t i = 0; i < difference; ++i){
+            (*this)[i + pos] = (*this)[i + pos + count];
+        }
+        _size = _size - count;
+    }
+}
+
+void Vector::eraseBetween(size_t beginPos, size_t endPos){
+    if (beginPos < endPos){
+        erase(beginPos, endPos - beginPos);
+    }
+}
+
 
 size_t Vector::size() const{
     return _size;
@@ -54,4 +226,11 @@ size_t Vector::size() const{
 
 size_t Vector::capacity() const{
     return _capacity;
+}
+
+void Vector::shrinkToFit(){
+    _capacity = _size;
+}
+double Vector::loadFactor() const{
+    return (double)_size/_capacity;
 }
